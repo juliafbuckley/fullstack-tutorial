b08126aaeb7054768170fadffcc0b72d980de0bc
juliafbuckley <juliafbuckley@gmail.com>
2023-02-06 15:34:28 -0500

    server, schema done

--- start/server/src/index.js
+++ start/server/src/index.js
@@ -1,1 +1,19 @@
+// Creating the server
 require('dotenv').config();
+// Import ApolloServer class from apollo-server
+const { ApolloServer } = require("apollo-server");
+// Import schema from schema.js
+const typeDefs = require("./schema");
+
+// Create a new instance of ApolloServer and passes it the 
+// imported schema 
+const server = new ApolloServer({ typeDefs });
+
+// Run the server
+server.listen().then(() => {
+    console.log(`
+        Server is running!
+        Listening on port 4000
+        Explore at https://studio.apollographql.com/sandbox
+        `);
+});
---
+++ start/server/src/schema.js
@@ -0,0 +1,72 @@
+// The schema defines what types of data a client
+// can read and write to your graph
+
+const { gql } = require("apollo-server");
+
+const typeDefs = gql `
+    type Launch {
+        # ! Means not null
+        id: ID!
+        site: String
+        mission: Mission
+        rocket: Rocket
+        isBooked: Boolean!
+    }
+
+    type Rocket {
+        id: ID!
+        name: String
+        type: String
+    }
+
+    type User {
+        id: ID!
+        email: String!
+        trips: [Launch]!
+        token: String
+    }
+
+    type Mission {
+        name: String
+        missionPatch(size: PatchSize): String
+    }
+
+    enum PatchSize {
+        SMALL
+        LARGE
+    }
+
+    # Define your graph's supported queries as fields of
+    # a special type called the Query type
+
+    type Query {
+        # returns array of all upcoming launches
+        launches: [Launch]!
+        # returns a single launch that corresponds to id
+        launch(id: ID!): Launch
+        # returns details for the User that is logged in
+        me: User
+    }
+
+    # Mutations enable clients to modify data
+
+    type Mutation {
+        # Enables user to book a trip on one or more launches
+        bookTrips(launchIds: [ID]!): TripUpdateResponse!
+        # Enables user to book a trip that they previously booked
+        cancelTrip(launchId: ID!): TripUpdateResponse!
+        # Enables user to log in with email
+        login(email: String): User
+    }
+
+    # Define a special object type for mutation response
+
+    type TripUpdateResponse {
+        success: Boolean!
+        message: String
+        launches: [Launch]
+    }
+    
+`;
+
+module.exports = typeDefs;
